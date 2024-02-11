feb7f82d0db63d43c7456e6d207c6197c868dccb
Avradeep Nayak <91881492+Zedoman@users.noreply.github.com>
2024-02-11 12:49:01 +0530

    Update Hero.tsx

--- client/app/Components/Route/Hero.tsx
+++ client/app/Components/Route/Hero.tsx
@@ -21,8 +21,13 @@ type Props = {}
         </div>
         <div className=' 800px:w-[50%] w-[80%] justify-center  flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[1px]'>
                 <h2 className='dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin  1000px:leading-[75px] 1500px:w-[600px]'>Improve Your Online Learning Experience Better Instantly</h2>
-                <p className='dark:text-white text-[#000000c7] text-[12px] px-3 w-full 1000px:text-[15px] font-[400] font-Josefin  1000px:leading-[30px] 1500px:w-[600px]'> We have 40k+ Online Courses & 500k+ Online registerd student.Find your desired Courses from them</p>
-            </div>
+                <p className='dark:text-white text-[#000000c7] text-[12px] px-3 w-full 1000px:text-[15px] font-[400] font-Josefin  1000px:leading-[30px] 1500px:w-[600px]'> We have 40k+ Online Courses & 500k+ Online registerd student.Find your desired Courses from them. And dont forget to try our study buddy to get help for your studies.</p>
+        <Link href='http://127.0.0.1:5000/'>
+          <button className='mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300 ease-in-out'>
+            Study Buddy
+          </button>
+        </Link>    
+        </div>
     </div>
   )
 }
